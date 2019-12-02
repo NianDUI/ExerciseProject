9e1fdd18204fe99961e88f9bede96424c6e18398
NianDUI <37703723+NianDUI@users.noreply.github.com>
2019-12-02 10:18:54 +0800

    2019年12月2日10:18:53

--- 01swagger/src/main/java/top/niandui/utils/CreateUUID.java
+++ 01swagger/src/main/java/top/niandui/utils/CreateUUID.java
@@ -7,8 +7,9 @@ import java.util.UUID;
  */
 public class CreateUUID {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println(UUID.randomUUID());
-        }
+//        for (int i = 0; i < 10; i++) {
+//            System.out.println(UUID.randomUUID());
+//        }
+        System.out.println(UUID.randomUUID());
     }
 }
