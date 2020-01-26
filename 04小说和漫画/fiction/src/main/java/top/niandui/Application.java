7252e697d6efe38116a488275e14ba368f92b5d9
NianDUI <37703723+NianDUI@users.noreply.github.com>
2020-01-26 21:42:47 +0800

    小说获取添加模块

--- 04小说和漫画/fiction/src/main/java/top/niandui/Application.java
+++ 04小说和漫画/fiction/src/main/java/top/niandui/Application.java
@@ -10,6 +10,13 @@ import java.util.*;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
+/**
+ * @Title: Application.java
+ * @description: Application
+ * @time: 2020/1/26 21:32
+ * @author: liyongda
+ * @version: 1.0
+ */
 public class Application {
 
     public static void main(String[] args) {
@@ -38,25 +45,31 @@ public class Application {
             }
             if (comName != null) {
                 Class<?> comClass = Class.forName("top.niandui.component." + comName);
-                Method main = comClass.getMethod("start");
-                main.invoke(comClass.newInstance());
+                Method startMethod = comClass.getMethod("start");
+                startMethod.invoke(comClass.newInstance());
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static List<String> getFiles(URL dirURL) throws IOException {
+    /**
+     * 根据组件所在url获取组件名称
+     * @param dirUrl 组件所在url
+     * @return       有哪些组件
+     * @throws IOException
+     */
+    public static List<String> getFiles(URL dirUrl) throws IOException {
         List<String> files = null;
-        String protocol = dirURL.getProtocol();
+        String protocol = dirUrl.getProtocol();
         if ("file".equals(protocol)) {
-            File dir = new File(dirURL.getFile());
-            files = Arrays.asList(dir.list((_dir, name) -> name.endsWith(".class")));
+            File dir = new File(dirUrl.getFile());
+            files = Arrays.asList(Objects.requireNonNull(dir.list((_dir, name) -> name.endsWith(".class"))));
         } else if ("jar".equals(protocol)) {
             files = new ArrayList<>();
-            String path = dirURL.getPath();
+            String path = dirUrl.getPath();
             path = path.substring(path.lastIndexOf("!/") + 2);
-            JarFile jarFile = ((JarURLConnection) dirURL.openConnection()).getJarFile();
+            JarFile jarFile = ((JarURLConnection) dirUrl.openConnection()).getJarFile();
             Enumeration<JarEntry> entries = jarFile.entries();
             boolean bl = false;
             while (entries.hasMoreElements()) {
