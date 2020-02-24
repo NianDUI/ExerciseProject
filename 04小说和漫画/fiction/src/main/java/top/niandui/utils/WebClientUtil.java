c6999e796c72c4846cf55b4aed6083e65f3a7331
NianDUI <37703723+NianDUI@users.noreply.github.com>
2020-02-24 14:33:36 +0800

    fiction调整

--- 04小说和漫画/fiction/src/main/java/top/niandui/utils/WebClientUtil.java
+++ 04小说和漫画/fiction/src/main/java/top/niandui/utils/WebClientUtil.java
@@ -153,11 +153,11 @@ public class WebClientUtil {
                 // 获取内容DOM列表
                 List list = htmlPage.getByXPath(info.contentXPathExpr);
                 for (int i = info.contentStartIndexOffset; i < list.size() + info.contentEndIndexOffset; i++) {
-                    String str = list.get(i).toString();
-                    if (str.trim().length() == 0) {
+                    String line = list.get(i).toString();
+                    if (line.trim().length() == 0) {
                         continue;
                     }
-                    sb.append(str).append(info.contentNewLine);
+                    sb.append(line).append(info.contentNewLine);
                 }
                 info.fileWriter.write(sb);
                 // 获取跳转超链接DOM列表
