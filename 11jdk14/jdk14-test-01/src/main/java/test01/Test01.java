d2443c2256e70d7b1082e212a3e75a3bf9cdcfbb
NianDUI <37703723+NianDUI@users.noreply.github.com>
2020-04-26 15:19:32 +0800

    springcloud:config

---
+++ 11jdk14/jdk14-test-01/src/main/java/test01/Test01.java
@@ -0,0 +1,18 @@
+package test01;
+
+import jdk.jshell.JShell;
+
+/**
+ * @Title: Test01.java
+ * @description: jdk14测试
+ * @time: 2020/4/24 16:55
+ * @author: liyongda
+ * @version: 1.0
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        JShell jShell = JShell.create();
+        jShell.eval("");
+//        Files.newBufferedReader()
+    }
+}
