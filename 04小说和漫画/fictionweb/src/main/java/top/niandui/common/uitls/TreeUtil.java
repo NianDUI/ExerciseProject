ffe7240e5899300b0447fa61467d7fd9126e7f11
NianDUI <37703723+NianDUI@users.noreply.github.com>
2020-07-21 14:06:31 +0800

    fictionweb：TreeUtil.java调整。

--- 04小说和漫画/fictionweb/src/main/java/top/niandui/common/uitls/TreeUtil.java
+++ 04小说和漫画/fictionweb/src/main/java/top/niandui/common/uitls/TreeUtil.java
@@ -7,79 +7,101 @@ import java.util.Iterator;
 import java.util.List;
 
 /**
- * 把一个list集合,里面的bean含有 id, parentId, children 转为树形式
+ * 把一个List<? extend IBaseTree>集合转为树形式
  */
 public class TreeUtil {
-	/**
-	 * 根据父节点的ID获取所有子节点
-	 * @param list 节点列表
-	 * @param praentId 传入的父节点ID
-	 * @return String
-	 */
-	public static <R, T extends IBaseTree<R, T>> List<T> getChildTreeObjects(List<T> list, R praentId) {
-		T t = (T) new IBaseTree<R, T>() {
-			private List<T> children = new ArrayList<T>();
 
-			@Override
-			public R getTreeId() {
-				return praentId;
-			}
+    /**
+     * 根据父节点的ID获取所有子节点
+     *
+     * @param list     节点列表
+     * @param praentId 传入的根节点ID
+     * @param <R>      节点id和父id的类型
+     * @param <T>      节点类型(IBaseTree的实现类)
+     * @return 树形处理后的列表
+     */
+    public static <R, T extends IBaseTree<R, T>> List<T> getChildTreeObjects(List<T> list, R praentId) {
+        T t = (T) new IBaseTree<R, T>() {
+            private List<T> children;
 
-			@Override
-			public R getTreeParentId() {
-				return null;
-			}
+            @Override
+            public R getTreeId() {
+                return praentId;
+            }
 
-			@Override
-			public void setChildren(List<T> children) {
-				this.children = children;
-			}
+            @Override
+            public R getTreeParentId() {
+                return null;
+            }
 
-			@Override
-			public List<T> getChildren() {
-				return children;
-			}
-		};
+            @Override
+            public void setChildren(List<T> children) {
+                this.children = children;
+            }
+
+            @Override
+            public List<T> getChildren() {
+                return children;
+            }
+        };
         // 递归所有节点
         recursionFn(list, t);
         return t.getChildren();
-	}
+    }
 
-	/**
-	 * 递归列表
-	 */
-	private static <R, T extends IBaseTree<R, T>> void recursionFn(List<T> list, T t) {
-		List<T> childList = getChildList(list, t);// 得到子节点列表
-		t.setChildren(childList);
-		for (T tChild : childList) {
-			if (hasChild(list, tChild)) {// 判断是否有子节点
-				//returnList.add(TreeObject);
-				Iterator<T> it = childList.iterator();
-				while (it.hasNext()) {
-					T n = it.next();
-					recursionFn(list, n);
-				}
-			}
-		}
-	}
+    /**
+     * 递归节点列表
+     *
+     * @param list 节点列表
+     * @param t    节点
+     * @param <R>  节点id和父id的类型
+     * @param <T>  节点类型(IBaseTree的实现类)
+     */
+    private static <R, T extends IBaseTree<R, T>> void recursionFn(List<T> list, T t) {
+        // 获取指定节点的子节点列表
+        List<T> childList = getChildList(list, t);
+        t.setChildren(childList);
+        for (T tChild : childList) {
+            // 判断指定节点是否有子节点
+            if (hasChild(list, tChild)) {
+                recursionFn(list, tChild);
+            }
+        }
+    }
 
-	// 得到子节点列表
-	private static <R, T extends IBaseTree<R, T>> List<T> getChildList(List<T> list, T t) {
-		List<T> tlist = new ArrayList<T>();
-		Iterator<T> it = list.iterator();
-		while (it.hasNext()) {
-			T n = it.next();
-			if (n.getTreeParentId().equals(t.getTreeId())) {
-				tlist.add(n);
-				// 匹配上则去除,优化性能
-				it.remove();
-			}
-		}
-		return tlist;
-	}
-	// 判断是否有子节点
-	private static <R, T extends IBaseTree<R, T>> boolean hasChild(List<T> list, T t) {
-//		return getChildList(list, t).size() > 0 ? true : false;
+    /**
+     * 获取指定节点的子节点列表
+     *
+     * @param list 节点列表
+     * @param t    节点
+     * @param <R>  节点id和父id的类型
+     * @param <T>  节点类型(IBaseTree的实现类)
+     * @return 子节点列表
+     */
+    private static <R, T extends IBaseTree<R, T>> List<T> getChildList(List<T> list, T t) {
+        List<T> tlist = new ArrayList<T>();
+        Iterator<T> it = list.iterator();
+        while (it.hasNext()) {
+            T n = it.next();
+            if (n.getTreeParentId().equals(t.getTreeId())) {
+                tlist.add(n);
+                // 匹配上则去除,优化性能
+                it.remove();
+            }
+        }
+        return tlist;
+    }
+
+    /**
+     * 判断指定节点是否有子节点
+     *
+     * @param list 节点列表
+     * @param t    节点
+     * @param <R>  节点id和父id的类型
+     * @param <T>  节点类型(IBaseTree的实现类)
+     * @return true有、false无
+     */
+    private static <R, T extends IBaseTree<R, T>> boolean hasChild(List<T> list, T t) {
         Iterator<T> it = list.iterator();
         while (it.hasNext()) {
             T n = it.next();
@@ -88,5 +110,5 @@ public class TreeUtil {
             }
         }
         return false;
-	}
+    }
 }
