ea2fa44700a03ce6a5f77179b2cd31799157c7db
NianDUI <760664212@qq.com>
2020-08-07 22:08:33 +0800

    fictionweb:sql更新。

--- 04小说和漫画/fictionweb/src/test/java/top/niandui/FictionwebApplicationTests.java
+++ 04小说和漫画/fictionweb/src/test/java/top/niandui/FictionwebApplicationTests.java
@@ -1,7 +1,10 @@
 package top.niandui;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.PrintStream;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 //@SpringBootTest
 class FictionwebApplicationTests {
@@ -10,4 +13,24 @@ class FictionwebApplicationTests {
     void contextLoads() {
     }
 
+    @Test
+    public void sqlHandleUtil() throws Exception {
+        Scanner sc = new Scanner(Paths.get("d:/public.sql"), "UTF-8");
+        PrintStream ps = new PrintStream("d:/fiction.sql");
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            if (line.startsWith("INSERT INTO \"public\".\"chapter\" VALUES")) {
+                continue;
+            }
+            if (line.startsWith("INSERT INTO \"public\".\"paragraph\" VALUES")) {
+                continue;
+            }
+            if (line.matches("INSERT INTO \"public\".\"paragraph_.*\" VALUES.*")) {
+                continue;
+            }
+            ps.println(line);
+        }
+        sc.close();
+        ps.close();
+    }
 }
