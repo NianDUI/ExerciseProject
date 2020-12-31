b6342a5f72a833817087221ca14b72d9f4d614aa
NianDUI <37703723+NianDUI@users.noreply.github.com>
2020-12-31 09:52:54 +0800

    添加全局异常不能拦截异常返回的错误属性。

---
+++ src/main/java/top/niandui/common/config/ErrorAttributes.java
@@ -0,0 +1,61 @@
+package top.niandui.common.config;
+
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.context.request.WebRequest;
+import top.niandui.common.expection.ReStateException;
+import top.niandui.common.expection.TokenCheckException;
+import top.niandui.common.outher.StatusCode;
+
+import javax.servlet.ServletException;
+import java.util.Map;
+
+/**
+ * 错误属性
+ *
+ * @author liyongda
+ * @version 1.0
+ * @date 2020/12/31 9:48
+ */
+@Component
+public class ErrorAttributes extends DefaultErrorAttributes {
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
+        Map<String, Object> error = super.getErrorAttributes(webRequest, options);
+        Throwable e = getError(webRequest);
+        if (e instanceof ServletException && e.getCause() != null) {
+            e = e.getCause();
+            while (e instanceof ServletException && e.getCause() != null) {
+                e = e.getCause();
+            }
+            // 添加返回code属性
+            error.put("code", getExceptionCode(e));
+            Object message = error.get("message");
+            if (message != null && message.toString().length() == 0) {
+                error.put("message", e.getMessage());
+            }
+        }
+        return error;
+    }
+
+    /**
+     * 获取错误相应的code码
+     *
+     * @param e 错误对象
+     * @return code码
+     */
+    private int getExceptionCode(Throwable e) {
+        if (e instanceof MethodArgumentNotValidException) {
+            return StatusCode.PARAM_FORMAT_ERROR;
+        } else if (e instanceof TokenCheckException) {
+            return StatusCode.TOKEN_ERROR;
+        } else if (e instanceof ReStateException) {
+            return StatusCode.RESTATE;
+        } else {
+            return StatusCode.EXECUTE_FAIL;
+        }
+    }
+
+}
