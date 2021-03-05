da6538cb3c843c24550bc437db90b83a80293be8
NianDUI <37703723+NianDUI@users.noreply.github.com>
2021-03-05 18:05:22 +0800

    日志相关调整

--- src/main/java/top/niandui/log/WebSocketAppender.java
+++ src/main/java/top/niandui/log/WebSocketAppender.java
@@ -1,8 +1,11 @@
 package top.niandui.log;
 
+import ch.qos.logback.classic.spi.LoggingEvent;
 import ch.qos.logback.core.Layout;
 import ch.qos.logback.core.UnsynchronizedAppenderBase;
 import ch.qos.logback.core.spi.DeferredProcessingAware;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.util.StringUtils;
 import top.niandui.websocket.LogWebSocketServer;
 
@@ -18,6 +21,8 @@ import java.util.concurrent.locks.ReentrantLock;
 public class WebSocketAppender<E> extends UnsynchronizedAppenderBase<E> {
     // 可重入锁
     private final ReentrantLock lock = new ReentrantLock(false);
+    // json格式化器
+    private final ObjectMapper json = new ObjectMapper();
     // 布局对象
     private Layout<E> layout;
 
@@ -41,6 +46,19 @@ public class WebSocketAppender<E> extends UnsynchronizedAppenderBase<E> {
         }
         // 格式化消息
         String txt = this.layout.doLayout(event);
+        // 将msg加入json字符串
+        if (event instanceof LoggingEvent) {
+            try {
+                LoggingEvent le = (LoggingEvent) event;
+                // 处理msg字符串为json字符串
+                txt = new StringBuilder(txt.substring(0, txt.length() - 1))
+                        .append(",\"msg\":")
+                        .append(json.writeValueAsString(le.getFormattedMessage()))
+                        .append(txt.substring(txt.length() - 1))
+                        .toString();
+            } catch (JsonProcessingException e) {
+            }
+        }
         writeString(txt);
     }
 
