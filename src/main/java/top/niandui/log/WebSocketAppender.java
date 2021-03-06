2b5dc3ed52561eaa016b3bb1eb52678a1da98d71
NianDUI <760664212@qq.com>
2021-03-06 13:16:48 +0800

    控制台日志处理调整

--- src/main/java/top/niandui/log/WebSocketAppender.java
+++ src/main/java/top/niandui/log/WebSocketAppender.java
@@ -37,6 +37,7 @@ public class WebSocketAppender<E> extends UnsynchronizedAppenderBase<E> {
      */
     @Override
     protected void append(E event) {
+        // 判断有没有开启处理
         if (!isStarted()) {
             return;
         }
@@ -44,22 +45,51 @@ public class WebSocketAppender<E> extends UnsynchronizedAppenderBase<E> {
         if (event instanceof DeferredProcessingAware) {
             ((DeferredProcessingAware) event).prepareForDeferredProcessing();
         }
+        // 处理事件，返回日志json字符串
+        String txt = processEvent(event);
+        // 写数据
+        writeString(txt);
+    }
+
+    /**
+     * 处理事件，返回日志json字符串
+     *
+     * @param event 事件对象
+     * @return 日志json字符串
+     */
+    private String processEvent(E event) {
         // 格式化消息
         String txt = this.layout.doLayout(event);
+        // 对ERROR中的错误信息详情进行处理
+        String errorDetail = "";
+        int lastIndex = txt.lastIndexOf('}') + 1;
+        if (lastIndex < txt.length()) {
+            // 截取错误详情
+            errorDetail = "<br/>" + txt.substring(lastIndex)
+                    .replaceAll("\r?\n", "<br/>")
+                    .replaceAll("\t", "&nbsp;&nbsp;&nbsp;&nbsp;");
+            // 截取json字符串
+            txt = txt.substring(0, lastIndex);
+        }
         // 将msg加入json字符串
         if (event instanceof LoggingEvent) {
             try {
                 LoggingEvent le = (LoggingEvent) event;
-                // 处理msg字符串为json字符串
+                // 处理响应json字符串
                 txt = new StringBuilder(txt.substring(0, txt.length() - 1))
+                        // 拼接信息字段
                         .append(",\"msg\":")
                         .append(json.writeValueAsString(le.getFormattedMessage()))
+                        // 拼接错误详情字段
+                        .append(",\"errorDetail\":")
+                        .append(json.writeValueAsString(errorDetail))
+                        // 拼接结束符
                         .append(txt.substring(txt.length() - 1))
                         .toString();
             } catch (JsonProcessingException e) {
             }
         }
-        writeString(txt);
+        return txt;
     }
 
     /**
