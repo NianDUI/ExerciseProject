18b25ba096c9f9079c7ea51e3dff3dffc47b3325
NianDUI <760664212@qq.com>
2021-04-02 21:13:44 +0800

    show.js bug调整

--- src/main/resources/static/js/chapter/show.js
+++ src/main/resources/static/js/chapter/show.js
@@ -3,11 +3,13 @@ setBaseParams({
     showUrl: base + "chapter/show/"
 });
 setAddIframeStyle();
-if (nextid != null) {
-    const thisIndex = baseParams.thisIndex;
+const thisIndex = baseParams.thisIndex;
+if (previd != null) {
     $(".prev").click(function () {
         parent.layer.iframeSrc(thisIndex, getBaseParams("showUrl", previd))
     });
+}
+if (nextid != null) {
     $(".next").click(function () {
         parent.layer.iframeSrc(thisIndex, getBaseParams("showUrl", nextid))
     });
