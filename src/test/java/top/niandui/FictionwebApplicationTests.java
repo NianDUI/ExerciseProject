1675b9f077c7d85533ede3600ad6a90a0ecf0913
NianDUI <760664212@qq.com>
2022-03-27 16:16:07 +0800

    备份

--- src/test/java/top/niandui/FictionwebApplicationTests.java
+++ src/test/java/top/niandui/FictionwebApplicationTests.java
@@ -15,8 +15,8 @@ class FictionwebApplicationTests {
 
     @Test
     public void navicatSqlHandleUtil() throws Exception {
-        Scanner sc = new Scanner(Paths.get("d:/public.sql"), "UTF-8");
-        PrintStream ps = new PrintStream("d:/fiction.sql");
+        Scanner sc = new Scanner(Paths.get("d:/ztest/public.sql"), "UTF-8");
+        PrintStream ps = new PrintStream("d:/ztest/fiction.sql");
         while (sc.hasNextLine()) {
             String line = sc.nextLine();
             if (line.startsWith("INSERT INTO \"public\".\"chapter\" VALUES")) {
@@ -42,8 +42,8 @@ class FictionwebApplicationTests {
         // # 备份和还原数据库全部信息
         // # pg_dumpall -h 127.0.0.1 -p 5432 -U postgres -c -f /soft/db/db_bak.sql
         // # psql -h 127.0.0.1 -p 5432 -U postgres -f /soft/db/db_bak.sql
-        Scanner sc = new Scanner(Paths.get("d:/fiction20210620"), "UTF-8");
-        PrintStream ps = new PrintStream("d:/fiction_cmd2.sql");
+        Scanner sc = new Scanner(Paths.get("d:/ztest/fiction20220327"), "UTF-8");
+        PrintStream ps = new PrintStream("d:/ztest/fiction_cmd2.sql");
         boolean skip = false;
         while (sc.hasNextLine()) {
             String line = sc.nextLine();
