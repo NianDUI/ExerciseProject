35a3852cb4a44182c6d087ffd9ab0b99575566e4
liyongda <liyongda@darana.cn>
2024-03-12 16:27:40 +0800

    日志打印调整

--- src/main/java/top/niandui/common/uitls/GetInfo.java
+++ src/main/java/top/niandui/common/uitls/GetInfo.java
@@ -31,13 +31,13 @@ public class GetInfo extends PropertyDefinerBase {
         String homePath = applicationHome.toString();
         if (homePath.endsWith("target\\classes")) {
             // 是开发环境，日志存放在项目目录下
-            LOG_DIR = homePath + "/../../log/";
+            LOG_DIR = homePath + "/../../log";
         } else if (homePath.endsWith("target")) {
             // 是命令行启动jar，日志存放在项目目录下
-            LOG_DIR = homePath + "/../log/";
+            LOG_DIR = homePath + "/../log";
         } else {
             // 不是开发环境，存放在启动目录下
-            LOG_DIR = homePath + "/";
+            LOG_DIR = homePath;
         }
     }
 
