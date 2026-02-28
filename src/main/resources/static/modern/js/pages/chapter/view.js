be7233648c4a40731c06fd8ab58d0af5aa210a68
liyongda <liyongda@darana.cn>
2026-02-28 09:43:31 +0800

    vue：js\pages\chapter 调整

--- src/main/resources/static/modern/js/pages/chapter/view.js
+++ src/main/resources/static/modern/js/pages/chapter/view.js
@@ -4,12 +4,17 @@ export default {
     name: "ChapterViewPage",
     template: `
     <div class="card">
+      <div class="toolbar">
+        <el-button @click="$router.back()">返回</el-button>
+        <el-button :disabled="!previd" @click="loadById(previd)">上一章</el-button>
+        <el-button :disabled="!nextid" @click="loadById(nextid)">下一章</el-button>
+      </div>
       <h3>{{ chapter.name }} <span v-if="book.name">- {{ book.name }}</span></h3>
       <el-divider />
       <div class="chapter-content">
         <p v-for="item in paragraphList" :key="item.seqid">{{ item.content }}</p>
       </div>
-      <div class="floating-actions">
+      <div class="toolbar" style="margin-top: 16px;">
         <el-button @click="$router.back()">返回</el-button>
         <el-button :disabled="!previd" @click="loadById(previd)">上一章</el-button>
         <el-button :disabled="!nextid" @click="loadById(nextid)">下一章</el-button>
@@ -29,6 +34,16 @@ export default {
         await this.loadById(Number(this.$route.params.id));
     },
     methods: {
+        scrollToTop() {
+            const container = document.querySelector(".main-scroll");
+            if (container) {
+                container.scrollTo({ top: 0, left: 0, behavior: "auto" });
+                container.scrollTop = 0;
+            }
+            document.documentElement.scrollTop = 0;
+            document.body.scrollTop = 0;
+            window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+        },
         async loadById(id) {
             if (!id) {
                 return;
@@ -42,6 +57,9 @@ export default {
             if (this.chapter.chapterid) {
                 this.$router.replace(`/chapter/view/${this.chapter.chapterid}`);
             }
+            this.$nextTick(() => {
+                this.scrollToTop();
+            });
         }
     }
 };
